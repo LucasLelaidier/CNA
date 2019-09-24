ba771169037dddcf1e23829ecb951e8895ac13b2
Lucas Lelaidier <lucaslelaidier@gmail.com>
2019-09-24 19:44:18 +0200

    gallery + start responsive

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -10,8 +10,40 @@ class GroupeAvenir extends Component {
             <body>
                 <Header logoPath="public\email.svg" />
 
-                <Footer />
+                <div className="container grey">
+                    <h1> Albums </h1>
 
+                    <section className="albums">
+                        <div className="album">
+                            <h2> Meeting du muguet 2018 </h2>
+                            <i className="dl-icon" data-feather="download"></i>
+                        </div>
+
+                        <div className="album">
+                            <h2> Championnat de normandie elbeuf 24 et 25 février 2018 </h2>
+                            <i className="dl-icon" data-feather="download"></i>
+                        </div>
+
+                        <div className="album">
+                            <h2> Meeting du muguet 2017 </h2>
+                            <i className="dl-icon" data-feather="download"></i>
+                        </div>
+                    </section>
+
+                    <h1> Dernières photos </h1>
+
+                    <section className="images">
+                        <div class="image" style={{backgroundImage: `url(https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60)`}}></div>
+                        <div class="image" style={{backgroundImage: `url(https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60)`}}></div>
+                        <div class="image" style={{backgroundImage: `url(https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60)`}}></div>
+                        <div class="image" style={{backgroundImage: `url(https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60)`}}></div>
+                        <div class="image" style={{backgroundImage: `url(https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60)`}}></div>
+                        <div class="image" style={{backgroundImage: `url(https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60)`}}></div>
+                        <div class="image" style={{backgroundImage: `url(https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60)`}}></div>
+                    </section>
+                </div>
+
+                <Footer />
             </body>
         )
     }
