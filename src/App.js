f2b3fa7f0d5e6228ef7a48cc8aa37d4232ccf534
Lucas Lelaidier <lucaslelaidier@gmail.com>
2019-09-28 18:43:44 +0200

    Improved responsive + fix header logo overflow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
+import feather from 'feather-icons'
 import MainRouter from './routes'
 import './global.css'
 
 class App extends Component {
+	componentDidMount() {
+        feather.replace();
+    }
+	
 	render() {
 		return (
 			<MainRouter />
